549078329134df427d84ce11c2c719e30675b59f
Chris Morris <chris.morris@scubago.com>
2020-09-08 10:20:18 +0100

    Added updates to database schema and functions, predominately around logging, auditing and configuration

---
+++ database/schema/V1_Schema.sql
@@ -0,0 +1,83 @@
+CREATE SCHEMA audit;
+
+CREATE SCHEMA configuration;
+
+CREATE SCHEMA logging;
+
+CREATE TABLE audit.entry (
+    entry_id integer NOT NULL,
+    ip character varying(1000) NOT NULL,
+    user_name character varying(255) NOT NULL,
+    ods_code character varying(255),
+    created_date date NOT NULL,
+    logged_on date,
+    logged_off date,
+    description character varying(1000) NOT NULL
+);
+
+CREATE TABLE audit.entry_type (
+    entry_type_id integer NOT NULL,
+    description character varying(1000) NOT NULL
+);
+
+
+CREATE TABLE configuration.entry (
+    entry_id integer NOT NULL,
+    key character varying(255) NOT NULL,
+    value character varying(1000) NOT NULL
+);
+
+CREATE TABLE logging.entry (
+    entry_id integer NOT NULL,
+    url character varying(1000) NOT NULL,
+    referrer_url character varying(1000),
+    description character varying(1000) NOT NULL,
+    ip character varying(255) NOT NULL,
+    created_date date NOT NULL,
+    created_by character varying(255) NOT NULL,
+    server character varying(255) NOT NULL,
+    response_code integer NOT NULL,
+    session_id character varying(1000) NOT NULL,
+    user_agent character varying(1000) NOT NULL
+);
+
+CREATE TABLE logging.entry_type (
+    entry_type_id integer NOT NULL,
+    description character varying(1000) NOT NULL
+);
+
+CREATE SEQUENCE audit.entry_entry_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE configuration.entry_entry_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE logging.entry_entry_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE ONLY audit.entry ALTER COLUMN entry_id SET DEFAULT nextval('audit.entry_entry_id_seq'::regclass);
+ALTER TABLE ONLY configuration.entry ALTER COLUMN entry_id SET DEFAULT nextval('configuration.entry_entry_id_seq'::regclass);
+ALTER TABLE ONLY logging.entry ALTER COLUMN entry_id SET DEFAULT nextval('logging.entry_entry_id_seq'::regclass);
+ALTER TABLE ONLY audit.entry ADD CONSTRAINT audit_entry_entryid_pk PRIMARY KEY (entry_id);
+ALTER TABLE ONLY audit.entry_type ADD CONSTRAINT audit_entrytype_entrytypeid_pk PRIMARY KEY (entry_type_id);
+ALTER TABLE ONLY configuration.entry ADD CONSTRAINT configuration_entry_entryid_pk PRIMARY KEY (entry_id);
+ALTER TABLE ONLY configuration.entry ADD CONSTRAINT unique_configuration_key UNIQUE (key);
+ALTER TABLE ONLY logging.entry ADD CONSTRAINT logging_entry_entryid_pk PRIMARY KEY (entry_id);
+ALTER TABLE ONLY logging.entry_type ADD CONSTRAINT logging_entrytype_entrytypeid_pk PRIMARY KEY (entry_type_id);
+ALTER TABLE ONLY audit.entry ADD CONSTRAINT audit_entry_entryid_fk FOREIGN KEY (entry_id) REFERENCES audit.entry_type(entry_type_id);
+ALTER TABLE ONLY logging.entry ADD CONSTRAINT logging_entry_entryid_fk FOREIGN KEY (entry_id) REFERENCES logging.entry_type(entry_type_id);
