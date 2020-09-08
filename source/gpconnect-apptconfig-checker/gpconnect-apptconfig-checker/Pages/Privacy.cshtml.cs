3f479b815b7fff7f7fc6dbdc761974157a9eae63
Chris Morris <chris.morris@scubago.com>
2020-09-08 09:37:46 +0100

    Initial commit of the boilerplate solution for GpConnect Appointment Configuration Checker

---
+++ source/gpconnect-apptconfig-checker/gpconnect-apptconfig-checker/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace gpconnect_apptconfig_checker.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
