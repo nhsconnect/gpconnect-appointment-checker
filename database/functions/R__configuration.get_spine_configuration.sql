a7945e3409fe520351764c5cd0ff411e3efbeef6
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2020-09-13 13:10:08 +0100

    Add configuration functions

---
+++ database/functions/R__configuration.get_spine_configuration.sql
@@ -0,0 +1,29 @@
+create or replace function configuration.get_spine_configuration
+(
+)
+returns table
+(
+    ssp_hostname varchar(100),
+    sds_hostname varchar(100),
+    sds_port integer,
+    sds_use_ldaps boolean,
+    organisation_id integer,
+    party_key varchar(20),
+    asid varchar(20)
+)
+as $$
+begin
+
+	return query
+	select
+	    s.ssp_hostname,
+	    s.sds_hostname,
+	    s.sds_port,
+	    s.sds_use_ldaps,
+	    s.organisation_id,
+	    s.party_key,
+	    s.asid
+	from configuration.spine s;
+	
+end;
+$$ language plpgsql;
