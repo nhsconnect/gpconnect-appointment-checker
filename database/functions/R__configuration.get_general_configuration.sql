259e171423f84fb8f7232c42bc31bf27ed3df165
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2020-10-19 11:49:07 +0100

    Remove audit retention period

--- database/functions/R__configuration.get_general_configuration.sql
+++ database/functions/R__configuration.get_general_configuration.sql
@@ -6,7 +6,6 @@ returns table
 	product_name varchar(100),
  	product_version varchar(100),
  	max_num_weeks_search smallint,
- 	audit_retention_days integer,
  	log_retention_days integer
 )
 as $$
@@ -17,7 +16,6 @@ begin
 	    g.product_name,
 	    g.product_version,
 	    g.max_num_weeks_search,
-	    g.audit_retention_days,
 	    g.log_retention_days
 	from configuration.general g;
 	
--- database/tests/T__test_1-configuration.get_general_configuration.sql
+++ database/tests/T__test_1-configuration.get_general_configuration.sql
@@ -2,7 +2,6 @@ select
     product_name,
     product_version,
     max_num_weeks_search,
-    audit_retention_period,
     log_retention_period
 from configuration.get_general_configuration
 (
