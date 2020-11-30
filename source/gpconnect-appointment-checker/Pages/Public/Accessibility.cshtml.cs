de62d89ff2d5ee1ca3ac4495024a7f45a127aab6
Chris Morris <chris.morris@scubago.com>
2020-11-30 10:11:38 +0000

    Added relevant footer links and content for the following:- 1) Accessibility statement #53 2) Help #54

---
+++ source/gpconnect-appointment-checker/Pages/Public/Accessibility.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace gpconnect_appointment_checker.Pages
+{
+    public class AccessibilityModel : PageModel
+    {
+        public string GetAccessEmailAddress { get; set; }
+        public string LastUpdated { get; set; }
+
+        protected IConfiguration _configuration;
+
+        public AccessibilityModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            GetAccessEmailAddress = _configuration["General:get_access_email_address"];
+            LastUpdated = $"{DateTime.UtcNow:MMMM yyyy}";
+        }
+    }
+}
