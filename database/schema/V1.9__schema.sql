6d8609f89ad71b904923694be99b4f5932ee88d7
Chris Morris <chris.morris@scubago.com>
2020-12-08 12:15:32 +0000

    Fix syntax error in the database script.

--- database/schema/V1.9__schema.sql
+++ database/schema/V1.9__schema.sql
@@ -2,7 +2,7 @@ alter table configuration.sds_query add column query_attributes varchar(500) nul
 
 update configuration.sds_query set query_attributes = 'nhsIDCode,o,postalAddress,postalCode,nhsOrgTypeCode' where query_name='GetOrganisationDetailsByOdsCode';
 update configuration.sds_query set query_attributes = 'nhsMHSPartyKey,uniqueIdentifier' where query_name='GetGpProviderAsIdByOdsCodeAndPartyKey';
-update configuration.sds_query set query_attributes = 'nhsMHSEndPoint,nhsMHSPartyKey,uniqueIdentifier where query_name='GetGpProviderEndpointAndPartyKeyByOdsCode';
+update configuration.sds_query set query_attributes = 'nhsMHSEndPoint,nhsMHSPartyKey,uniqueIdentifier' where query_name='GetGpProviderEndpointAndPartyKeyByOdsCode';
 
 alter table configuration.sds_query alter column query_attributes set not null;
 
