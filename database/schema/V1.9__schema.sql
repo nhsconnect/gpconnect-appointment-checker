f4cbf75a990287d3de84f0af43eba3adfa87622d
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2020-12-08 11:13:02 +0000

    Fix patch

--- database/schema/V1.9__schema.sql
+++ database/schema/V1.9__schema.sql
@@ -1,6 +1,6 @@
 alter table configuration.sds_query add column query_attributes varchar(500) null;
 
-alter table configuration.spine add constraint configuration_sdsquery_queryattributes_ck check (char_length(trim(query_attributes)) > 0);
+alter table configuration.sds_query add constraint configuration_sdsquery_queryattributes_ck check (char_length(trim(query_attributes)) > 0);
 
 update configuration.sds_query
 set query_attributes = 'nhsIDCode,o,postalAddress,postalCode,nhsOrgTypeCode' 
