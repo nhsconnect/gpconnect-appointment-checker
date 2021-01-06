0b7246b968b9d227de958fb8bea1bf6522e6b7ba
Chris Morris <chris.morris@scubago.com>
2021-01-06 13:50:35 +0000

    Initial changes to indicate whether or not an appointment slot is in the past.

--- source/gpconnect-appointment-checker.DTO/Response/GpConnect/SlotEntrySimple.cs
+++ source/gpconnect-appointment-checker.DTO/Response/GpConnect/SlotEntrySimple.cs
@@ -17,11 +17,23 @@ namespace gpconnect_appointment_checker.DTO.Response.GpConnect
         public string PractitionerRole { get; set; }
         public string PractitionerGender { get; set; }
 
+        public bool PastSlot => IsInPast(AppointmentDate, StartTime);
+
         public string LocationName { get; set; }
         public List<string> LocationAddressLines { get; set; }
         public string LocationCity { get; set; }
         public string LocationDistrict { get; set; }
         public string LocationPostalCode { get; set; }
         public string LocationCountry { get; set; }
+
+        private bool IsInPast(DateTime? appointmentDate, DateTime? startTime)
+        {
+            if (appointmentDate != null && startTime != null)
+            {
+                return DateTime.Now > new DateTime(appointmentDate.Value.Year, appointmentDate.Value.Month,
+                    appointmentDate.Value.Day, startTime.Value.Hour, startTime.Value.Minute, startTime.Value.Second);
+            }
+            return false;
+        }
     }
 }
