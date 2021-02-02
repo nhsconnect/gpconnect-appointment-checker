8acd94d56b97843bfad4be7b2e119f6e9bde189c
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2021-02-02 13:18:34 +0000

    Fix patch V2.2

--- database/schema/V2.2__schema.sql
+++ database/schema/V2.2__schema.sql
@@ -1,13 +1,3 @@
--- create user
 create user dbpatcher_user;
 alter user dbpatcher_user valid until 'infinity';
-
--- grant all permissions
-do
-$$
-declare 
-  dbname text := current_database();
-begin
-  execute format('grant all privileges on database "%s" to dbpatcher_user', dbname);
-end
-$$;
+grant postgres to dbpatcher_user;
