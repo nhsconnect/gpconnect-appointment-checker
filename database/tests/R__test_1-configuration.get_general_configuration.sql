5ab945fd65b4f3eda4699acf77a3b84abbef72b3
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2021-02-08 15:58:34 +0000

    Fix SQL test

--- database/tests/R__test_1-configuration.get_general_configuration.sql
+++ database/tests/R__test_1-configuration.get_general_configuration.sql
@@ -2,7 +2,8 @@ select
     product_name,
     product_version,
     max_num_weeks_search,
-    log_retention_days
+    log_retention_days,
+    get_access_email_address
 from configuration.get_general_configuration
 (
 );
