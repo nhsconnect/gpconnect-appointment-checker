a836998af2c0a88113a36b4261c9729da7fa5b55
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2021-03-04 15:21:36 +0000

    Create stored functions to add a new user and list all users #129

---
+++ database/functions/R__application.get_users.sql
@@ -0,0 +1,37 @@
+drop function if exists application.get_users;
+
+create function application.get_users
+(
+)
+returns table
+(
+	user_id integer, 
+	email_address varchar(200), 
+	display_name varchar(200), 
+	organisation_id integer,
+	is_authorised boolean,
+	added_date timestamp,
+	authorised_date timestamp,
+	last_logon_date timestamp
+)
+as $$
+begin
+
+	--------------------------------------------
+	-- return user data
+	--
+	return query
+	select
+		u.user_id,
+		u.email_address,
+		u.display_name,
+		u.organisation_id,
+		u.is_authorised,
+		u.added_date,
+		u.authorised_date,
+		u.last_logon_date
+	from application.user u
+	order by u.user_id;
+	
+end;
+$$ language plpgsql;
