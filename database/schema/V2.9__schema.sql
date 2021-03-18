38e3892c4818311b31921918fd1daff182cf938a
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2021-03-18 14:42:44 +0000

    Add reporting stored functions #137

--- database/schema/V2.9__schema.sql
+++ database/schema/V2.9__schema.sql
@@ -1,1 +1,9 @@
 create schema reporting;
+
+grant usage on schema reporting to app_user;
+grant select, insert, update on all tables in schema reporting to app_user;
+grant select, update on all sequences in schema reporting to app_user;
+grant execute on all functions in schema reporting to app_user;
+
+grant usage on schema reporting to readonly_user;
+grant select on all tables in schema reporting to readonly_user;
