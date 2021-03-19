83f7a5a1593a176bccbac671537cbb6804b463de
Chris Morris <chris.morris13@nhs.net>
2021-03-19 09:01:00 +0000

    Added a basic admin page and model

---
+++ source/gpconnect-appointment-checker/Models/AdminModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gpconnect_appointment_checker.Models
+{
+    public class AdminModel
+    {
+    }
+}
---
+++ source/gpconnect-appointment-checker/Pages/Private/Admin/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using gpconnect_appointment_checker.Configuration.Infrastructure.Logging.Interface;
+using gpconnect_appointment_checker.DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace gpconnect_appointment_checker.Pages
+{
+    public partial class AdminModel : PageModel
+    {
+        protected IConfiguration _configuration;
+        protected IHttpContextAccessor _contextAccessor;
+        protected ILogger<AdminModel> _logger;
+        protected IApplicationService _applicationService;
+        protected IAuditService _auditService;
+        protected readonly ILoggerManager _loggerManager;
+
+        public AdminModel(IConfiguration configuration, IHttpContextAccessor contextAccessor, ILogger<AdminModel> logger, IApplicationService applicationService, IAuditService auditService, ILoggerManager loggerManager = null)
+        {
+            _configuration = configuration;
+            _contextAccessor = contextAccessor;
+            _logger = logger;
+            _applicationService = applicationService;
+            _auditService = auditService;
+            if (null != loggerManager)
+            {
+                _loggerManager = loggerManager;
+            }
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
