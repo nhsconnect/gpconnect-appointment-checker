a5017d120b3fbeb82a343f3f5683e4f3f8943fa0
Chris Morris <chris.morris13@nhs.net>
2021-03-26 16:54:25 +0000

    #125 Added help content for the multiple code search

--- source/gpconnect-appointment-checker/Pages/Public/Help/Help.cshtml.cs
+++ source/gpconnect-appointment-checker/Pages/Public/Help/Help.cshtml.cs
@@ -1,3 +1,4 @@
+using gpconnect_appointment_checker.Helpers;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 
@@ -6,6 +7,7 @@ namespace gpconnect_appointment_checker.Pages
     public class HelpModel : PageModel
     {
         public string GetAccessEmailAddress { get; set; }
+        public int MaxNumberConsumerCodesSearch { get; set; }
 
         protected IConfiguration _configuration;
 
@@ -17,6 +19,7 @@ namespace gpconnect_appointment_checker.Pages
         public void OnGet()
         {
             GetAccessEmailAddress = _configuration["General:get_access_email_address"];
+            MaxNumberConsumerCodesSearch = _configuration["General:max_number_consumer_codes_search"].StringToInteger();
         }
     }
 }
