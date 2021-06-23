567f3018625028c56d25baaeeb2c3b70dcc91e23
Chris Morris <chris.morris13@nhs.net>
2021-06-23 12:32:49 +0100

    Fix to the DB test

--- database/tests/R__test_9-application.logoff_user.sql
+++ database/tests/R__test_9-application.logoff_user.sql
@@ -45,7 +45,7 @@ begin
 		_organisation_id := _organisation_id
 	);
 
-	update application.user set user_account_status_id = 2 where email_address = _email_address;
+	update application.user u set user_account_status_id = 2 where u.email_address = _email_address;
 
 	select
 		us.user_session_id into _user_session_id
