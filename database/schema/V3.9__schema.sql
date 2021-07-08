cf75d715b4d2a14fbc121d5bcf75d2d5376ca26d
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2021-07-08 16:29:31 +0100

    #143 Changes to add and use a new sds query for consumer enablement

--- database/schema/V3.9__schema.sql
+++ database/schema/V3.9__schema.sql
@@ -1,20 +1,14 @@
-DO $$
-BEGIN
-	IF NOT EXISTS (SELECT FROM configuration.sds_query WHERE query_name='GetGpConsumerAsIdByOdsCode') THEN
-		INSERT INTO configuration.sds_query 
-		(
-			query_name, 
-			search_base, 
-			query_text, 
-			query_attributes
-		)
-		VALUES 
-		(
-			'GetGpConsumerAsIdByOdsCode',
-			'ou=services, o=nhs',
-			'(&(nhsIDCode={odsCode})(objectClass=nhsAs)(nhsAsSvcIA=urn:nhs:names:services:gpconnect:fhir:rest:read:metadata-1))',
-			'uniqueIdentifier'
-		);
-	END IF;
-END
-$$;
+INSERT INTO configuration.sds_query 
+(
+	query_name, 
+	search_base, 
+	query_text, 
+	query_attributes
+)
+VALUES 
+(
+	'GetGpConsumerAsIdByOdsCode',
+	'ou=services, o=nhs',
+	'(&(nhsIDCode={odsCode})(objectClass=nhsAs)(nhsAsSvcIA=urn:nhs:names:services:gpconnect:fhir:rest:read:metadata-1))',
+	'uniqueIdentifier'
+);
