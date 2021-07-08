21ef8acf0cfad622531797426715670646c69d8d
Chris Morris <chris.morris13@nhs.net>
2021-07-08 10:41:31 +0100

    #167 Fix to the DB schema for audit entries

--- database/functions/R__audit.add_entry.sql
+++ database/functions/R__audit.add_entry.sql
@@ -5,9 +5,9 @@ create function audit.add_entry
     _user_id integer,
     _user_session_id integer,
     _entry_type_id integer,
-    _item1 varchar(100) default null,
-    _item2 varchar(100) default null,
-    _item3 varchar(100) default null,
+    _item1 varchar(1000) default null,
+    _item2 varchar(1000) default null,
+    _item3 varchar(1000) default null,
     _details varchar(1000) default null,
     _entry_elapsed_ms integer default null,
     _admin_user_id integer default null
