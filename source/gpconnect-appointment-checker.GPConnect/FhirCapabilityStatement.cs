27c459add8fccd385c8eeec7ccdcea26e30f5b2e
Chris Morris <chris.morris13@nhs.net>
2021-07-13 11:33:54 +0100

    #169 Spine message log is added when httpclient exception occurs.

--- source/gpconnect-appointment-checker.GPConnect/FhirCapabilityStatement.cs
+++ source/gpconnect-appointment-checker.GPConnect/FhirCapabilityStatement.cs
@@ -103,15 +103,14 @@ namespace gpconnect_appointment_checker.GPConnect
         private async Task<CapabilityStatement> GetCapabilityStatement(RequestParameters requestParameters, string baseAddress)
         {
             var getRequest = new HttpRequestMessage();
+            var stopWatch = new Stopwatch();           
+            
             try
             {
                 var spineMessageType = (_configurationService.GetSpineMessageTypes()).FirstOrDefault(x =>
                     x.SpineMessageTypeId == (int)SpineMessageTypes.GpConnectReadMetaData);
                 requestParameters.SpineMessageTypeId = (int)SpineMessageTypes.GpConnectReadMetaData;
                 requestParameters.InteractionId = spineMessageType?.InteractionId;
-
-                var stopWatch = new Stopwatch();
-                stopWatch.Start();
 
                 _spineMessage.SpineMessageTypeId = requestParameters.SpineMessageTypeId;
 
@@ -122,16 +121,15 @@ namespace gpconnect_appointment_checker.GPConnect
                 getRequest.Method = HttpMethod.Get;
                 getRequest.RequestUri = new Uri($"{AddSecureSpineProxy(baseAddress, requestParameters)}/metadata");
 
+                _spineMessage.RequestPayload = getRequest.ToString();
+                
+                stopWatch.Start();
                 var response = await client.SendAsync(getRequest);
                 var responseStream = await response.Content.ReadAsStringAsync();
 
                 _spineMessage.ResponsePayload = responseStream;
-                _spineMessage.ResponseStatus = response.StatusCode.ToString();
-                _spineMessage.RequestPayload = getRequest.ToString();
-                _spineMessage.ResponseHeaders = response.Headers.ToString();
-                stopWatch.Stop();
-                _spineMessage.RoundTripTimeMs = stopWatch.ElapsedMilliseconds;
-                _logService.AddSpineMessageLog(_spineMessage);
+                _spineMessage.ResponseStatus = response.StatusCode.ToString();                
+                _spineMessage.ResponseHeaders = response.Headers.ToString();                           
 
                 var results = JsonConvert.DeserializeObject<CapabilityStatement>(responseStream);
                 return results;
@@ -140,6 +138,12 @@ namespace gpconnect_appointment_checker.GPConnect
             {
                 _logger.LogError(exc, $"An error occurred in trying to execute a GET request - {getRequest}");
                 throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _spineMessage.RoundTripTimeMs = stopWatch.ElapsedMilliseconds;
+                _logService.AddSpineMessageLog(_spineMessage);
             }
         }
     }
