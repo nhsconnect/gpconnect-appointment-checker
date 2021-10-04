2dc660b4e550b11dde20ce3dcc0dbef6e5cfc41e
Chris Morris <chris.morris13@nhs.net>
2021-10-04 15:40:23 +0100

    #199 Fix to the report for returning stats by the number of ods codes

--- database/functions/R__reporting.get_search_by_number_of_ods_code.sql
+++ database/functions/R__reporting.get_search_by_number_of_ods_code.sql
@@ -15,21 +15,39 @@ returns table
 )
 as $$
 begin
+	create temp table table_total_response_times on commit drop as		
+	select
+		a.provider_ods_count, 
+		a.consumer_ods_count, 
+		avg(a.response_time_in_seconds) average_total_response_time_in_seconds
+	from 
+	(
+		select 
+			array_length(string_to_array(sg.provider_ods_textbox, ' '), 1) provider_ods_count,
+			coalesce(array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1), 0) consumer_ods_count,
+			sum(sm.roundtriptime_ms)/1000 response_time_in_seconds		
+		from
+			application.search_group sg
+			inner join application.search_result sr on sg.search_group_id = sr.search_group_id			
+			inner join logging.spine_message sm on sr.search_result_id = sm.search_result_id
+		group by
+			sg.search_group_id
+	) a 
+	group by 
+		a.provider_ods_count,
+		a.consumer_ods_count;
+	
 	return query
 	select
-		a.number_of_provider_codes,
-		a.number_of_consumer_codes,
-		a.count,
-		to_char(a.average_total_response_time, 'FM9990.99"s"'),
-		to_char(a.average_response_per_code, 'FM9990.99"s"')
+		* 
 	from 
 	(
 		select
-			1 "number_of_provider_codes",
-			1 "number_of_consumer_codes",
-			count(*) "count",
-			round(sum(roundtriptime_ms)/1000 / count(*), 2) "average_response_per_code",
-			round(sum(roundtriptime_ms)/1000 / count(*), 2) "average_total_response_time"
+			1,
+			1,
+			count(*),
+			to_char(sum(roundtriptime_ms)/1000 / count(*), 'FM9990.99"s"'),
+			to_char(sum(roundtriptime_ms)/1000 / count(*), 'FM9990.99"s"')
 		from
 			logging.spine_message sm
 		where
@@ -38,11 +56,11 @@ begin
 			and position('ods-organization-code' in sm.request_payload) > 0
 		union
 		select
-			1 "number_of_provider_codes",
-			0 "number_of_consumer_codes",
-			count(*) "count",
-			round(sum(roundtriptime_ms)/1000 / count(*), 2) "average_response_per_code",
-			round(sum(roundtriptime_ms)/1000 / count(*), 2) "average_total_response_time"
+			1,
+			0,
+			count(*),
+			to_char(sum(roundtriptime_ms)/1000 / count(*), 'FM9990.99"s"'),
+			to_char(sum(roundtriptime_ms)/1000 / count(*), 'FM9990.99"s"')
 		from
 			logging.spine_message sm
 		where
@@ -52,42 +70,28 @@ begin
 			and position('GPConnect-OrganisationType' in sm.request_payload) > 0
 		union
 		select 
-			array_length(string_to_array(sg.provider_ods_textbox, ' '), 1) "number_of_provider_codes",
-			array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1) "number_of_consumer_codes",
-			count(*) "count",
-			round(sum(sm.roundtriptime_ms)/1000 / greatest(array_length(string_to_array(sg.provider_ods_textbox, ' '), 1), array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1)), 2) "average_response_per_code",
-			round(sum(sm.roundtriptime_ms)/1000, 2) "average_total_response_time"
-		from
-			logging.spine_message sm
-			inner join application.search_result sr on sm.search_result_id = sr.search_result_id
-			inner join application.search_group sg on sr.search_group_id = sg.search_group_id
-		where
-			sm.spine_message_type_id = 3 
-			and sm.search_result_id is not null
-			and position('ods-organization-code' in sm.request_payload) > 0
-		group by
-			array_length(string_to_array(sg.provider_ods_textbox, ' '), 1),
-			array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1)
-		union
-		select 
-			array_length(string_to_array(sg.provider_ods_textbox, ' '), 1),
-			coalesce(array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1), 0),
-			count(*),
-			round(sum(sm.roundtriptime_ms)/1000 / greatest(array_length(string_to_array(sg.provider_ods_textbox, ' '), 1), array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1)), 2) "average_response_per_code",
-			round(sum(sm.roundtriptime_ms)/1000, 2) "average_total_response_time"
-		from
-			logging.spine_message sm
-			inner join application.search_result sr on sm.search_result_id = sr.search_result_id
-			inner join application.search_group sg on sr.search_group_id = sg.search_group_id
-		where
-			sm.spine_message_type_id = 3 
-			and sm.search_result_id is not null
-			and position('ods-organization-code' in sm.request_payload) = 0
-			and position('GPConnect-OrganisationType' in sm.request_payload) > 0
-		group by
-			array_length(string_to_array(sg.provider_ods_textbox, ' '), 1),
-			array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1)
-	) a 
-	order by 2,1,3;
+			a.provider_ods_count,
+			a.consumer_ods_count,
+			a.count,
+			to_char(t.average_total_response_time_in_seconds, 'FM9990.99"s"'),
+			to_char(a.average_response_per_code_in_seconds, 'FM9990.99"s"')		
+		from 
+		(
+			select 
+				array_length(string_to_array(sg.provider_ods_textbox, ' '), 1) provider_ods_count,
+				coalesce(array_length(string_to_array(sg.consumer_ods_textbox, ' '), 1), 0) consumer_ods_count,
+				count(*) "count",
+				avg(sm.roundtriptime_ms)/1000 average_response_per_code_in_seconds
+			from
+				application.search_group sg		
+				inner join application.search_result sr on sg.search_group_id = sr.search_group_id			
+				inner join logging.spine_message sm on sr.search_result_id = sm.search_result_id
+			group by
+				1,2
+		) a 
+	inner join table_total_response_times t on a.provider_ods_count = t.provider_ods_count and a.consumer_ods_count = t.consumer_ods_count
+	) b
+	order by
+		1,2;
 end;
 $$ language plpgsql;
