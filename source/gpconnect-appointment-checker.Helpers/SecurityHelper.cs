a8bd240b29746a60d787fa716d436b9eca2c6507
ChristopherJamesMorris <hello@christopherjmorris.com>
2022-04-28 10:44:01 +0100

    #235 Removed support for older TLS versions

--- source/gpconnect-appointment-checker.Helpers/SecurityHelper.cs
+++ source/gpconnect-appointment-checker.Helpers/SecurityHelper.cs
@@ -7,9 +7,6 @@ namespace gpconnect_appointment_checker.Helpers
         public static SslProtocols ParseTlsVersion(string tlsVersion) =>
             tlsVersion switch
             {
-                "1.0" or "Tls10" => SslProtocols.Tls,
-                "1.1" or "Tls11" => SslProtocols.Tls11,
-                "1.2" or "Tls12" => SslProtocols.Tls12,
                 "1.3" or "Tls13" => SslProtocols.Tls13,
                 _ => SslProtocols.None,
             };
