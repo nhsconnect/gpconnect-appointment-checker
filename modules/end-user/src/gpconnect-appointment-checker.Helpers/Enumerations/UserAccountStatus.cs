509449a2a870ed3ee75829149db6c0b872f13413
Chris Morris <chris.morris13@nhs.net>
2023-09-15 16:22:21 +0100

    Removed usings

--- modules/end-user/src/gpconnect-appointment-checker.Helpers/Enumerations/UserAccountStatus.cs
+++ modules/end-user/src/gpconnect-appointment-checker.Helpers/Enumerations/UserAccountStatus.cs
@@ -1,5 +1,4 @@
-﻿using DocumentFormat.OpenXml.Wordprocessing;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace gpconnect_appointment_checker.Helpers.Enumerations
