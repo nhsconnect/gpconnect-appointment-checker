1cd6f9cbe8c8423ac7c859bd139a873f6f1f6876
Chris Morris <chris.morris13@nhs.net>
2024-02-12 09:42:44 +0000

    Taking config values from secrets

---
+++ modules/function/src/Configuration/SecretManager.cs
@@ -0,0 +1,38 @@
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace GpConnect.AppointmentChecker.Function.Configuration;
+
+public class SecretManager
+{
+    public string Get(string secretName)
+    {
+        var config = new AmazonSecretsManagerConfig { RegionEndpoint = RegionEndpoint.EUWest2 };
+        var client = new AmazonSecretsManagerClient(config);
+
+        var request = new GetSecretValueRequest
+        {
+            SecretId = $"gpcac/{secretName}"
+        };        
+
+        GetSecretValueResponse response = null;
+        try
+        {
+            response = Task.Run(async () => await client.GetSecretValueAsync(request)).Result;
+        }
+        catch (ResourceNotFoundException)
+        {
+            Console.WriteLine("The requested secret " + secretName + " was not found");
+        }
+        catch (InvalidRequestException e)
+        {
+            Console.WriteLine("The request was invalid due to: " + e.Message);
+        }
+        catch (InvalidParameterException e)
+        {
+            Console.WriteLine("The request had invalid params: " + e.Message);
+        }
+        return response?.SecretString;
+    }    
+}
