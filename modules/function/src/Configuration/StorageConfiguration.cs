2c0bc5d7c3847f4872560a4dfcecec624339b456
Chris Morris <chris.morris13@nhs.net>
2024-02-14 12:23:34 +0000

    Added changes to take list of ods codes from source object in s3 bucket

--- modules/function/src/Configuration/StorageConfiguration.cs
+++ modules/function/src/Configuration/StorageConfiguration.cs
@@ -3,4 +3,5 @@ namespace GpConnect.AppointmentChecker.Function.Configuration;
 internal class StorageConfiguration
 {
     public string BucketName { get; set; }
+    public string SourceObject { get; set; }
 }
