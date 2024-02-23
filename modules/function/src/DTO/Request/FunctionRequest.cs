37d3523da9dc6fdbdf8174d9b9ae409bac2520d7
Chris Morris <chris.morris13@nhs.net>
2024-02-23 14:30:28 +0000

    Finalisation event logic

--- modules/function/src/DTO/Request/FunctionRequest.cs
+++ modules/function/src/DTO/Request/FunctionRequest.cs
@@ -4,5 +4,4 @@ public class FunctionRequest
 {
     public List<string> DistributionList { get; set; }
     public List<string> OdsCodes { get; set; } = new List<string>();
-    public string ReportName { get; set; }
 }
