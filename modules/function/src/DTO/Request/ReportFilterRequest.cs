c229c72a2509cc9601be9873a1a759ecfe393439
Chris Morris <chris.morris13@nhs.net>
2024-03-12 10:33:40 +0000

    Added input tag to completion function

--- modules/function/src/DTO/Request/ReportFilterRequest.cs
+++ modules/function/src/DTO/Request/ReportFilterRequest.cs
@@ -4,4 +4,5 @@ public class ReportFilterRequest
 {
     public string? FilterColumn { get; set; } = null;
     public string? FilterValue { get; set; } = null;
+    public string? FilterTab { get; set; } = null;
 }
