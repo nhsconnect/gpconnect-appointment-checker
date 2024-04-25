fc7b20ef34012ee0f53af1251dfccc3d9ebac369
Chris Morris <chris.morris13@nhs.net>
2024-04-25 12:28:44 +0100

    Updated func

--- database/functions/R__reporting.get_capability_reports.sql
+++ database/functions/R__reporting.get_capability_reports.sql
@@ -6,18 +6,20 @@ create function reporting.get_capability_reports
 returns table
 (
 	report_name varchar(100), 
-	interaction json
+	interaction json,
+	workflow json
 )
 as $$
 begin
 	return query
 	select
 		r.report_name,
-		r.interaction
+		r.interaction,
+		r.workflow
 	from 
 		reporting.list r
 	where 
-		r.interaction is not null
-		and r.function_name is null;	
+		(r.interaction is not null or r.workflow is not null)
+		and r.function_name is null;
 end;
 $$ language plpgsql;
