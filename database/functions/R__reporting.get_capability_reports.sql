1021eb84ed1b4e43c15e9b29a548b902a093356b
Chris Morris <chris.morris13@nhs.net>
2024-04-25 12:32:11 +0100

    Minor tweak

--- database/functions/R__reporting.get_capability_reports.sql
+++ database/functions/R__reporting.get_capability_reports.sql
@@ -13,13 +13,13 @@ as $$
 begin
 	return query
 	select
-		r.report_name,
-		r.interaction,
-		r.workflow
+		rl.report_name,
+		rl.interaction,
+		rl.workflow
 	from 
-		reporting.list r
+		reporting.list rl
 	where 
-		(r.interaction is not null or r.workflow is not null)
-		and r.function_name is null;
+		(rl.interaction is not null or rl.workflow is not null)
+		and rl.function_name is null;
 end;
 $$ language plpgsql;
