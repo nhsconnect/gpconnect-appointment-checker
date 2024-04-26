752be927a0e0f2557a2d228ad8a25dd199ea1790
Chris Morris <chris.morris13@nhs.net>
2024-04-26 11:22:31 +0100

    Fix to object key filename

--- modules/function/src/DTO/Request/ReportInteraction.cs
+++ modules/function/src/DTO/Request/ReportInteraction.cs
@@ -7,7 +7,21 @@ public class ReportInteraction
     public List<ReportSource> ReportSource { get; set; } = null;
     public List<string>? Interaction { get; set; }
     public List<string>? Workflow { get; set; }
-    public string ObjectKeyJson => $"{Helpers.Constants.Objects.Transient}_{ReportName?.ReplaceNonAlphanumeric()}_{DateTime.Now.ToString("s").ReplaceNonAlphanumeric()}_{ Guid.NewGuid() }_{Interaction[0]?.ReplaceNonAlphanumeric() }.json".ToLower();
+    public string ObjectKeyJson => $"{Helpers.Constants.Objects.Transient}_{ReportName?.ReplaceNonAlphanumeric()}_{DateTime.Now.ToString("s").ReplaceNonAlphanumeric()}_{GetTypeLabel(Interaction, Workflow)}_{ Guid.NewGuid() }.json".ToLower();
+
+    private string GetTypeLabel(List<string>? interaction, List<string>? workflow)
+    {
+        if (interaction != null && interaction[0] != null)
+        {
+            return interaction[0].ReplaceNonAlphanumeric();
+        }
+        if (workflow != null && workflow[0] != null)
+        {
+            return workflow[0].ReplaceNonAlphanumeric();
+        }
+        return string.Empty;
+    }
+
     public string? ReportName { get; set; } = null;
     public string? PreSignedUrl { get; set; } = null;
 }
