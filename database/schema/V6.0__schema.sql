5340673bd8befd34e5cc206d10b9511b73a97fc8
Chris Morris <chris.morris13@nhs.net>
2024-05-01 12:36:18 +0100

    Fix to schema

--- database/schema/V6.0__schema.sql
+++ database/schema/V6.0__schema.sql
@@ -1,4 +1,4 @@
-alter table reporting.list add column report_id character varying(100)
+alter table reporting.list add column report_id character varying(100);
 
 update reporting.list set report_id='AccessRecordStructured' where report_name='Access Record: Structured';
 update reporting.list set report_id='AccessRecordHTML' where report_name='Access Record: HTML';
