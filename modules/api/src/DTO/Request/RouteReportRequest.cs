0c3c7ac51a23ca069c278b9baa6a4396d333db5c
Chris Morris <chris.morris13@nhs.net>
2024-05-12 18:30:10 +0100

    Debugging

--- modules/api/src/DTO/Request/RouteReportRequest.cs
+++ modules/api/src/DTO/Request/RouteReportRequest.cs
@@ -5,8 +5,8 @@ public class RouteReportRequest
     public List<ReportSource> ReportSource { get; set; }
     public List<string>? Interaction { get; set; }
     public List<string>? Workflow { get; set; }
-    public bool IsInteraction => Interaction != null && Interaction.Any() && Workflow == null;
+    //public bool IsInteraction => Interaction != null && Interaction.Any() && Workflow == null;
     public string? ReportName { get; set; } = null;
     public string? ReportId { get; set; } = null;
-    public Guid? MessageGroupId { get; set; } = Guid.NewGuid();
+    //public Guid? MessageGroupId { get; set; } = Guid.NewGuid();
 }
