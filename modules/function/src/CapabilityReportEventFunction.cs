0bbb160a435769585ce7e3c1e5429e14bb962e10
Chris Morris <chris.morris13@nhs.net>
2024-06-19 12:42:26 +0100

    Mods

--- modules/function/src/CapabilityReportEventFunction.cs
+++ modules/function/src/CapabilityReportEventFunction.cs
@@ -140,9 +140,6 @@ public class CapabilityReportEventFunction
         //    Encoding.UTF8,
         //    MediaTypeHeaderValue.Parse("application/json").MediaType);
 
-        //_lambdaContext.Logger.LogLine("await json.ReadAsStringAsync()");
-        //_lambdaContext.Logger.LogLine(await json.ReadAsStringAsync());
-
         //var response = await _httpClient.PostWithHeadersAsync("/reporting/createinteractionmessage", new Dictionary<string, string>()
         //{
         //    [Headers.UserId] = _endUserConfiguration.UserId,
