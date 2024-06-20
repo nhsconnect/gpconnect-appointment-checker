a741e4d6f9aca781abb041a85fadf3b27798a791
Chris Morris <chris.morris13@nhs.net>
2024-06-20 14:47:53 +0100

    Wrong dto reference

--- modules/api/src/DTO/Request/ReportInteractionRequest.cs
+++ modules/api/src/DTO/Request/ReportInteractionRequest.cs
@@ -2,7 +2,7 @@ namespace GpConnect.AppointmentChecker.Api.DTO.Request;
 
 public class ReportInteractionRequest
 {
-    public DataSource DataSource { get; set; }
+    public List<DataSource> DataSource { get; set; }
     public List<string>? Interaction { get; set; }
     public List<string>? Workflow { get; set; }
     public string? ReportName { get; set; } = null;
