998ac4c9d8eb5ea4fbea5dbe876e82f569350a7d
Chris Morris <chris.morris13@nhs.net>
2024-06-24 11:23:20 +0100

    Trying to initiate a build

--- database/schema/V6.6__schema.sql
+++ database/schema/V6.6__schema.sql
@@ -2,8 +2,8 @@ drop table if exists reporting.transient;
 
 create table reporting.transient
 (
-    transient_id varchar(100) not null,
-    transient_data json not null,
+	transient_id varchar(100) not null,
+	transient_data json not null,
 	transient_report_id varchar(100) not null,
 	transient_report_name varchar(100) not null,
 	entry_date timestamp without time zone not null
