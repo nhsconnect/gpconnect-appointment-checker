858ada313a9be1b99a239b31590155dfba95c354
Grant Riordan <12433404+grant-dot-dev@users.noreply.github.com>
2025-05-07 13:53:55 +0100

    GCS-279 fix admin page error when no results / cache

--- modules/api/src/Helpers/LoggingHelper.cs
+++ modules/api/src/Helpers/LoggingHelper.cs
@@ -6,20 +6,21 @@ namespace GpConnect.AppointmentChecker.Api.Helpers
     {
         public static int GetIntegerValue(string header)
         {
-            if(LogManager.Configuration.Variables[header] != null)
+            if (LogManager.Configuration.Variables.Count > 0)
             {
-                return LogManager.Configuration.Variables[header].ToString().StringToInteger(0);
+                return LogManager.Configuration.Variables?[header] != null
+                    ? LogManager.Configuration.Variables[header].ToString().StringToInteger(0)
+                    : 0;
             }
+
             return 0;
         }
 
         public static string GetStringValue(string header)
         {
-            if (LogManager.Configuration.Variables[header] != null)
-            {
-                return LogManager.Configuration.Variables[header].ToString();
-            }
-            return string.Empty;
+            return LogManager.Configuration.Variables[header] != null
+                ? LogManager.Configuration.Variables[header].ToString()
+                : string.Empty;
         }
     }
 }
--- modules/api/src/Helpers/PagingHelpers.cs
+++ modules/api/src/Helpers/PagingHelpers.cs
@@ -1,11 +1,19 @@
 namespace gpconnect_appointment_checker.api.Helpers;
 
-public class PagingHelpers
+public static class PagingHelpers
 {
     public static TItem[][] Paginate<TItem>(IEnumerable<TItem> originalItems, int pageSize = 50)
     {
+        ArgumentNullException.ThrowIfNull(originalItems);
+
         var itemsArray = originalItems as TItem[] ?? originalItems.ToArray();
-        var totalPages = (int)Math.Ceiling((double)itemsArray.Count() / pageSize);
+
+        if (itemsArray.Length == 0)
+        {
+            return [];
+        }
+
+        var totalPages = (int)Math.Ceiling((double)itemsArray.Length / pageSize);
         var pages = new List<TItem[]>();
 
         for (var pageNumber = 0; pageNumber < totalPages; pageNumber++)
